aefcb60c9be688db83a523e84f3a01fc2a71bcf8
Léo Alves <leolorena.alves@gmail.com>
2024-11-09 14:13:15 -0300

    alterações (tirei só uns espaços)

--- src/service/cliente/clienteAddService.js
+++ src/service/cliente/clienteAddService.js
@@ -1,6 +1,6 @@
 import {addCliente}from "../../repository/clienteRepository.js";
+
 export default async function clienteAddService(cliente){
-    
     let idGerado = await addCliente(cliente);
     return idGerado;
 }
